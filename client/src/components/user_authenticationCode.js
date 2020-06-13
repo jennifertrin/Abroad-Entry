b20ad303167d794edb761a6aee56fb712e17eed3
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-06-13 18:40:02 -0400

    added style changes
    Added style changes to FlightCode and AuthenticationCode

--- client/src/components/user_authenticationCode.js
+++ client/src/components/user_authenticationCode.js
@@ -29,8 +29,8 @@ class AuthenticationCode extends Component {
                 </div>
                 <div className="jumbotron jumbotron-fluid is-fullheight">
                     <div className="container absolutelyOneThird">
-                        <h2>Thank you for flying with us, {this.state.airline}, from {this.state.origin} to {this.state.destination}.</h2> <br/>
-                        <h4>For verification that you are a passenger, please enter your passport #.</h4> <br />
+                        <h2>Thank you for flying with us, {this.state.airline}, from {this.state.origin} to {this.state.destination}.</h2>
+                        <h4>For verification that you are a passenger, please enter your passport #.</h4>
                         <input type="text" className="form-control mr-50" id="passportNumber" />
                         <br /> <br />
                         <button type="button" class="Submit btn-lg" onClick={() => this.authenticateCode(document.getElementById("passportNumber").value)}>Submit</button>
--- client/src/components/user_flightCode.js
+++ client/src/components/user_flightCode.js
@@ -47,21 +47,19 @@ class FlightCode extends Component {
                 <div id="welcomeScreen">
                     <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
                         <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"></path>
-                        <text x="50%" y="10%" id="companyHeader">Welcome to Abroad Entry!</text>
+                        <text x="50%" y="15%" id="companyHeader">Welcome to Abroad Entry!</text>
                     </svg>
                 </div>
-                    <div className="container absolutelyOneThird">
-                    <div class="panelTravel">
+                <div class="panelTravel">
                         <h2>Please enter the code provided by your flight attendants.</h2>
-                    </div>
-                        <div class="input-group mb-3 input-group-lg code-input">
-                            <div class="input-group-prepend">
+                </div>
+                    <div class="input-group mb-3 input-group-lg code-input col-sm-4">
+                        <div class="input-group-prepend">
                                 <span class="input-group-text" id="codeSpan">Code</span>
                             </div>
                             <input type="text" class="form-control" placeholder="Example: A8B6F6" id="flightCode" />
                         </div>
                         <button type="button" class="Submit btn-lg" onClick={() => this.validateCode(document.getElementById("flightCode").value)}>Submit</button>
-                    </div>
                 
             </div>
         );
