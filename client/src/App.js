b049152ac16fb5a13fabe186dc8aa5f4eec5007a
Edward <echang49@uwo.ca>
2020-06-13 17:34:18 -0400

    Express Fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+ } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
 import FlightCode from './components/user_flightCode';
 import AuthenticationCode from './components/user_authenticationCode';
 import Summary from './components/user_summary';
@@ -11,12 +17,29 @@ import FormCanada from './components/form_Canada';
 function App() {
   return (
     <div className="App">
-      
-      {/*<AuthenticationCode />*/}
-      {/*<Summary />*/}
-      {/*<FlightAttendantCode />*/}
-      {/*<FlightAttendantInput />*/}
-      <FormCanada />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <FlightCode />
+          </Route>
+          <Route path="/auth">
+            <AuthenticationCode />
+          </Route>
+          <Route path="/form/Canada">
+            <FormCanada />
+          </Route>
+          {/**Medical Records should be a component of the forms */}
+          <Route path="/summary">
+            <Summary />
+          </Route>
+          <Route path="/flight/form">
+            <FlightAttendantCode />
+          </Route>
+          <Route path="/flight/code">
+            <FlightAttendantInput />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,29 +1,78 @@
 const express = require('express');
 const path = require('path');
+const e = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
+
+let exampleData = {
+    "airline" : "United Airlines",
+	"flightNumber" : "3513",
+	"flightCode" : "A8F3C9",
+	"originAirport" : "YYZ",
+	"destinationAirport" : "JFK",
+	"passengerForms" : [
+		{
+			"Passport": "A832DF",
+		},{
+			"Passport": "B832DF",
+		},{
+			"Passport": "C832DF",
+		},{
+			"Passport": "D832DF",
+		},
+	]
+}
+
 //Validates the flight code
-app.get('/api/user/codeValidation', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
-    console.log('Sent list of items');
+app.post('/api/user/codeValidation', (req,res) => {
+    //console.log(exampleData.flightCode);
+    //console.log(req.body.code);
+    if(exampleData.flightCode == req.body.code){ //validated
+        res.status(200).send({"result": true});
+        //Should merge passportInfo into here to send back the data which could be moved within react
+    }
+    else{ //not validated
+        res.status(200).send({"result": false});
+    }
 });
 
 //Returns flight information
 app.get('/api/user/passportInfo', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
+    //later req will find the flightData
+    res.json({
+        airline : exampleData.airline,
+        originAirport : exampleData.originAirport,
+        destinationAirport : exampleData.destinationAirport
+    });
     console.log('Sent list of items');
 });
 
 //Validates the passport number as a passenger of the plane
 app.get('/api/user/passportValidation', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
+    let validation = false;
+    for(let i = 0; i < exampleData.passengerForms.length ; i++){
+        if(req == exampleData.passengerForms[i].Passport){
+            validation = true;
+            break;
+        }
+    }
+    if(validation){
+        res.send(200, {"result": true});
+    }
+    else{
+        res.send(200, {"result": false});
+    }
     console.log('Sent list of items');
 });
 
@@ -36,50 +85,15 @@ app.get('/api/user/medicalEncrypt', (req,res) => {
 
 //Submits the user form
 app.get('/api/user/formSubmit', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
-    console.log('Sent list of items');
+    //req.post to api
 });
 
 //Submits the flight attendant form
 app.get('/api/flight/formSubmit', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
-    console.log('Sent list of items');
-});
-
-//Recieves the flight code
-app.get('/api/flight/getCode', (req,res) => {
-    /*var list = ["item1", "item2", "item3"];
-    res.json(list);*/
-    console.log('Sent list of items');
+    //req.post to api, make a random code and send back the code as well as pushing to api
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
-
-let exampleData = {
-    "airline" : "UA",
-	"flightNumber" : "3513",
-	"flightCode" : "A8F3C9",
-	"originAirport" : "YYZ",
-	"destinationAirport" : "JFK",
-	"passengerForms" : [
-		{
-			"Passport#": "A832DF",
-		},{
-			"Passport#": "B832DF",
-		},{
-			"Passport#": "C832DF",
-		},{
-			"Passport#": "D832DF",
-		},
-	]
-}
